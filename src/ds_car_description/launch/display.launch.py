4812d7e56c9399532a9bb10ecb93823c086159ce
parkdosol <parkdosol1@gmail.com>
2025-01-31 15:40:17 +0900

    (에러 O) rviz 상에서 띄우는 것 까진 성공.. 근데 완벽하지 않아서 tf 써볼 생각..

--- src/ds_car_description/launch/display.launch.py
+++ src/ds_car_description/launch/display.launch.py
@@ -3,21 +3,23 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from ament_index_python.packages import get_package_share_directory
-from launch.substitutions import Command
+from launch.substitutions import Command, PathJoinSubstitution
 
 def generate_launch_description():
-    urdf_file = os.path.join(
+    urdf_file = PathJoinSubstitution([
         get_package_share_directory("ds_car_description"),
         "urdf",
         "ds_car.urdf.xacro"
-    )
+    ])
 
     return LaunchDescription([
         Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
             parameters=[{
-                "robot_description": ParameterValue(Command(["xacro", urdf_file]), value_type=str)
+                "robot_description": ParameterValue(
+                    Command(["xacro ", urdf_file]), value_type=str
+                )
             }]
         ),
         Node(
